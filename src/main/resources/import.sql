3c414001cc536994b92f5fb0ffe856cf1b46b7f6
hkhajgiwale <harsh.khajgiwale@gmail.com>
2018-12-07 01:05:45 +0530

    added the files

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,13 @@
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 1','user 1');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 2','user 1');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 3','user 1');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 4','user 2');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 5','user 2');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 6','user 2');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 7','user 3');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 8','user 3');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 9','user 3');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 10','user 4');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 11','user 4');
-INSERT INTO messages(message_description, message_from) VALUES ('This is message 12','user 4');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 1','user_1');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 2','user_1');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 3','user_1');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 4','user_2');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 5','user_2');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 6','user_2');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 7','user_3');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 8','user_3');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 9','user_3');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 10','user_4');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 11','user_4');
+INSERT INTO messages(message_description, message_from) VALUES ('This is message 12','user_4');
 
